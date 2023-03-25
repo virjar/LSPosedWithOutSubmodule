ac6f5ba8d381d1f28b7bf8bebab7323128d456d6
virjar <virjar@virjar.com>
2023-03-25 15:05:44 +0800

    sync 20230325

--- app/src/main/java/org/lsposed/manager/ui/activity/base/BaseActivity.java
+++ app/src/main/java/org/lsposed/manager/ui/activity/base/BaseActivity.java
@@ -52,6 +52,7 @@ public class BaseActivity extends MaterialActivity {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         // make sure the versions are consistent
         if (BuildConfig.DEBUG) return;
--- services/xposed-service/interface/src/main/aidl/io/github/xposed/xposedservice/IXposedService.aidl
+++
@@ -1,5 +0,0 @@
-package io.github.xposed.xposedservice;
-
-interface IXposedService {
-    int getVersion() = 1;
-}
