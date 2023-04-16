7710e9eee9e98c05d399d2a6286ec56d210e11ef
virjar <virjar@virjar.com>
2023-04-16 10:32:50 +0800

    sync

---
+++ external/lsplant/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "7.1.2"
+        id("com.android.library") version "7.1.2"
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LSPlant"
+include(
+    ":lsplant",
+    ":test",
+)
