33885d99f01c3d92e84ce01fa3501a81837baf4f
virjar <virjar@virjar.com>
2023-04-16 10:43:16 +0800

    sync

---
+++ external/lsplant/test/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.application")
+}
+
+val androidTargetSdkVersion: Int by rootProject.extra
+val androidMinSdkVersion: Int by rootProject.extra
+val androidBuildToolsVersion: String by rootProject.extra
+val androidCompileSdkVersion: Int by rootProject.extra
+val androidNdkVersion: String by rootProject.extra
+val androidCmakeVersion: String by rootProject.extra
+
+android {
+    compileSdk = androidCompileSdkVersion
+    ndkVersion = androidNdkVersion
+    buildToolsVersion = androidBuildToolsVersion
+
+    buildFeatures {
+        buildConfig = false
+        prefab = true
+    }
+
+    defaultConfig {
+        applicationId = "org.lsposed.lsplant"
+        minSdk = androidMinSdkVersion
+        targetSdk = androidTargetSdkVersion
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        externalNativeBuild {
+            cmake {
+                arguments += "-DANDROID_STL=c++_shared"
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/jni/CMakeLists.txt")
+            version = androidCmakeVersion
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    namespace = "org.lsposed.lsplant"
+}
+
+dependencies {
+    implementation(project(":lsplant"))
+    implementation("io.github.vvb2060.ndk:dobby:1.2")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test:runner:1.4.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
